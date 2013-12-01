6461514f1260cf98312ad229e178d84fbe6ded39
Nicolas Gallagher <nicolasgallagher@gmail.com>
2013-11-30 23:09:24 -0800

    Create granular tests
    Use 'chai' for assertions so that `Error` messages can be checked.

--- test/test.js
+++ test/test.js
@@ -1,17 +1,41 @@
 var fs = require('fs');
 var rework = require('rework');
-var should = require('should');
+var expect = require('chai').expect;
 var vars = require('..')();
 
 function fixture(name){
   return fs.readFileSync('test/fixtures/' + name + '.css', 'utf8').trim();
 }
 
+function compareFixtures(name){
+  return expect(
+    rework(fixture(name))
+    .use(vars)
+    .toString().trim()
+  ).to.equal(fixture(name + '.out'));
+}
+
 describe('rework-vars', function(){
-  it('should replace variables', function(){
-    rework(fixture('original'))
-      .use(vars)
-      .toString().trim()
-      .should.equal(fixture('expected'));
+  it('removes variable properties from the output', function(){
+    compareFixtures('remove-properties');
+  });
+
+  it('throws an error when a variable function references an undefined variable', function(){
+    var output = function () {
+      return rework(fixture('substitution-undefined')).use(vars).toString();
+    };
+    expect(output).to.Throw(Error, 'rework-vars: variable "test" is undefined');
+  });
+
+  it('substitutes defined variables', function(){
+    compareFixtures('substitution-defined');
+  });
+
+  it('overwrites variables in place', function(){
+    compareFixtures('substitution-overwrite');
+  });
+
+  it('supports case-sensitive variables', function(){
+    compareFixtures('case-sensitive');
   });
 });
