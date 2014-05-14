6b9394ad4b624d6018e6f32d679d5a4e5aa615a2
Nicolas Gallagher <nicolasgallagher@gmail.com>
2014-05-14 08:36:19 -0700

    Use 'balanced-match' module for var() parsing
    Fix gh-14

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
  * Module dependencies.
  */
 
+var balanced = require('balanced-match');
 var visit = require('rework-visit');
 
 /**
  * Constants.
  */
 
-var VAR_SET_IDENTIFIER = '--';
-var VAR_GET_IDENTIFIER = 'var(';
+var VAR_PROP_IDENTIFIER = '--';
+var VAR_FUNC_IDENTIFIER = 'var';
 
 /**
  * Module export.
@@ -25,30 +26,29 @@ module.exports = function(jsmap) {
       var varNameIndices = [];
 
       if (rule.type !== 'rule') return;
-
       // only variables declared for `:root` are supported
-      if (rule.selectors.length === 1 && rule.selectors[0] === ':root') {
-        rule.declarations.forEach(function(decl, idx){
-          var prop = decl.property;
-          var val = decl.value;
+      if (rule.selectors.length !== 1 || rule.selectors[0] !== ':root') return;
 
-          if (prop && prop.indexOf(VAR_SET_IDENTIFIER) === 0) {
-            map[prop] = val;
-            varNameIndices.push(idx);
-          }
-        });
+      rule.declarations.forEach(function(decl, idx){
+        var prop = decl.property;
+        var val = decl.value;
 
-        // remove `--*` properties from the rule
-        for (var i = varNameIndices.length - 1; i >= 0; i -= 1) {
-          rule.declarations.splice(varNameIndices[i], 1);
+        if (prop && prop.indexOf(VAR_PROP_IDENTIFIER) === 0) {
+          map[prop] = val;
+          varNameIndices.push(idx);
         }
+      });
+
+      // remove `--*` properties from the rule
+      for (var i = varNameIndices.length - 1; i >= 0; i -= 1) {
+        rule.declarations.splice(varNameIndices[i], 1);
       }
     });
 
     // resolve variables
     visit(style, function(declarations, node){
       declarations.forEach(function(decl, idx){
-        if (decl.value && decl.value.indexOf(VAR_GET_IDENTIFIER) !== -1) {
+        if (decl.value && decl.value.indexOf(VAR_FUNC_IDENTIFIER + '(') !== -1) {
           decl.value = replaceValue(decl.value, map);
         }
       });
@@ -65,43 +65,23 @@ module.exports = function(jsmap) {
  *
  * var(name[, fallback])
  *
- * Since the fallback can be *any* value, the value needs to be parsed
- * character-by-character to deduce the contents of a variable function.
- *
  * @param {String} value A property value known to contain CSS variable functions
  * @param {Object} map A map of variable names and values
  * @return {String} A property value with all CSS variables substituted.
  */
 
 function replaceValue(value, map){
-  // matches `var(name[, fallback])`, captures 'name' and 'fallback'
-  var RE_VAR = /\bvar\(([\w-]+)(?:\s*,\s*)?(.*)?\)/;
-  // matches `var()`
-  var RE_EMPTY_VAR = /\bvar\(\s*\)/;
+  // matches `name[, fallback]`, captures 'name' and 'fallback'
+  var RE_VAR = /([\w-]+)(?:\s*,\s*)?(.*)?/;
+  var balancedParens = balanced('(', ')', value);
+  var varRef = balanced(VAR_FUNC_IDENTIFIER + '(', ')', value).body;
 
-  var valueLen = value.length;
-  var beginSlice = value.indexOf('var(');
-  var endSlice = beginSlice + 'var('.length;
-  var depth = 1;
-  var currentChar;
-  var cssVariable;
+  if (!balancedParens) throw new Error('rework-vars: missing closing ")" in the value "' + value + '"');
+  if (varRef === '') throw new Error('rework-vars: var() must contain a non-whitespace string');
 
-  // find the closing `)` of the CSS variable function,
-  // accounting for nested functions
-  while (endSlice < valueLen && depth > 0) {
-    currentChar = value.charAt(endSlice);
-    if (currentChar == '(') depth += 1;
-    if (currentChar == ')') depth -= 1;
-    endSlice += 1;
-  }
+  var varFunc = VAR_FUNC_IDENTIFIER + '(' + varRef + ')';
 
-  if (depth > 0) throw new Error('rework-vars: missing closing ")" in the value "' + value + '"');
-
-  cssVariable = value.slice(beginSlice, endSlice);
-
-  if (RE_EMPTY_VAR.test(cssVariable)) throw new Error('rework-vars: var() must contain a non-whitespace string');
-
-  cssReplacement = cssVariable.replace(RE_VAR, function(_, name, fallback){
+  var varResult = varRef.replace(RE_VAR, function(_, name, fallback){
     var replacement = map[name];
     if (!replacement && !fallback) throw new Error('rework-vars: variable "' + name + '" is undefined');
     if (!replacement && fallback) return fallback;
@@ -109,10 +89,10 @@ function replaceValue(value, map){
   });
 
   // resolve the variable
-  value = value.split(cssVariable).join(cssReplacement);
+  value = value.split(varFunc).join(varResult);
 
-  // recursively resolve any remaining variables
-  if (value.indexOf(VAR_GET_IDENTIFIER) !== -1) {
+  // recursively resolve any remaining variables in the value
+  if (value.indexOf(VAR_FUNC_IDENTIFIER) !== -1) {
     value = replaceValue(value, map);
   }
 
